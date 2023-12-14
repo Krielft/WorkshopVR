d61062379ca26df4182eea49309122c32af58f15
Galaan <galaan.rouzier@e-artsup.net>
2023-12-14 14:15:55 +0100

    Scene Update

--- Assets/Scripts/TimerSystem/Restart.cs
+++ Assets/Scripts/TimerSystem/Restart.cs
@@ -4,7 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
-    public void Replay()
+    public void Replay1()
+    {
+        SceneManager.LoadScene("Scene final");
+        Time.timeScale = 1;
+    }
+
+    public void Replay2()
     {
         SceneManager.LoadScene("Galaan");
         Time.timeScale = 1;
